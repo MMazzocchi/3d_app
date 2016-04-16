36d39caa1d1402576d0194048cf04eda702b8358
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2016-04-16 06:34:55 -0600

    Okay, rolling back the changes to a good base version.

--- js/code.js
+++ js/code.js
@@ -9,20 +9,6 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// Make a cube
-var geometry = new THREE.PlaneGeometry( 100, 1000, 10, 10 );
-var material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
-var plane = new THREE.Mesh( geometry, material );
-//plane.position.z = -10;
-plane.position.y = -10;
-plane.rotation.x = -Math.PI/2.0;
-scene.add(plane);
-
-// Make a light
-var light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(0, 0, 0);
-scene.add(light);
-
 var controls = undefined;
 
 function setOrientationControls(e) {
@@ -41,9 +27,6 @@ window.addEventListener('deviceorientation', setOrientationControls, true);
 // Render
 var render = function () {
     requestAnimationFrame( render );
-
-//    cube.rotation.x += 0.1;
-//    cube.rotation.y += 0.1;
 
     camera.updateProjectionMatrix();
     if(controls) controls.update(clock.getDelta());
